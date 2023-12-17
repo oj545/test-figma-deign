df5387e04828713f648800250a47837ced8177a2
oj545 <106318515+oj545@users.noreply.github.com>
2023-12-17 16:09:29 +0200

    fixed some css

--- src/App.js
+++ src/App.js
@@ -4,164 +4,164 @@ import Navbar from "./components/UI/navbar";
 import MarketingSection from "./components/sections/MarketingSection";
 import AboutSection from "./components/sections/AboutSection";
 import ServicesSection from "./components/sections/ServicesSection";
-const bubbles = [
-  {
-    size: 10,
-    position: { top: 7, left: 50 },
-    rotate: 100,
-    color1: "#F8B5D3",
-    color2: "#E278FF",
-  },
-  {
-    size: 8,
-    position: { top: 35, left: 35 },
-    rotate: 30,
+// const bubbles = [
+//   {
+//     size: 10,
+//     position: { top: 7, left: 50 },
+//     rotate: 100,
+//     color1: "#F8B5D3",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 8,
+//     position: { top: 35, left: 35 },
+//     rotate: 30,
 
-    color1: "#E278FF",
-    color2: "#BBF3FD",
-  },
-  {
-    size: 15,
-    position: { top: 18, left: 8 },
-    rotate: 100,
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
-  {
-    size: 20,
-    position: { top: 70, left: 20 },
-    rotate: 30,
-    color1: "#F8B5D3",
-    color2: "#E278FF",
-  },
-  {
-    size: 15,
-    position: { top: 55, left: 70 },
-    rotate: 80,
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
-  {
-    size: 10,
-    position: { top: 107, left: 50 },
-    rotate: 100,
-    color1: "#F8B5D3",
-    color2: "#E278FF",
-  },
-  {
-    size: 8,
-    position: { top: 118, left: 8 },
-    rotate: 100,
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
+//     color1: "#E278FF",
+//     color2: "#BBF3FD",
+//   },
+//   {
+//     size: 15,
+//     position: { top: 18, left: 8 },
+//     rotate: 100,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 20,
+//     position: { top: 70, left: 20 },
+//     rotate: 30,
+//     color1: "#F8B5D3",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 15,
+//     position: { top: 55, left: 70 },
+//     rotate: 80,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 10,
+//     position: { top: 107, left: 50 },
+//     rotate: 100,
+//     color1: "#F8B5D3",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 8,
+//     position: { top: 118, left: 8 },
+//     rotate: 100,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
 
-  {
-    size: 8,
-    position: { top: 150, left: 40 },
-    rotate: 100,
-    color1: "#F8B5D3",
-    color2: "#E278FF",
-  },
-  {
-    size: 18,
-    position: { top: 190, left: 20 },
-    rotate: 30,
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
-  {
-    size: 17,
-    position: { top: 165, left: 80 },
-    rotate: 80,
+//   {
+//     size: 8,
+//     position: { top: 150, left: 40 },
+//     rotate: 100,
+//     color1: "#F8B5D3",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 18,
+//     position: { top: 190, left: 20 },
+//     rotate: 30,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 17,
+//     position: { top: 165, left: 80 },
+//     rotate: 80,
 
-    color1: "#F8B5D3",
-    color2: "#E278FF",
-  },
-  {
-    size: 10,
-    position: { top: 207, left: 50 },
-    rotate: 100,
-    color1: "#F8B5D3",
-    color2: "#E278FF",
-  },
-  {
-    size: 8,
-    position: { top: 218, left: 8 },
-    rotate: 100,
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
+//     color1: "#F8B5D3",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 10,
+//     position: { top: 207, left: 50 },
+//     rotate: 100,
+//     color1: "#F8B5D3",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 8,
+//     position: { top: 218, left: 8 },
+//     rotate: 100,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
 
-  {
-    size: 8,
-    position: { top: 250, left: 40 },
-    rotate: 100,
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
-  {
-    size: 18,
-    position: { top: 290, left: 20 },
-    rotate: 9,
+//   {
+//     size: 8,
+//     position: { top: 250, left: 40 },
+//     rotate: 100,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 18,
+//     position: { top: 290, left: 20 },
+//     rotate: 9,
 
-    color1: "#F8B5D3",
-    color2: "#E278FF",
-  },
-  {
-    size: 12,
-    position: { top: 240, left: 10 },
-    rotate: 30,
+//     color1: "#F8B5D3",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 12,
+//     position: { top: 240, left: 10 },
+//     rotate: 30,
 
-    color1: "#E278FF",
-    color2: "#BBF3FD",
-  },
-  {
-    size: 17,
-    position: { top: 265, left: 60 },
-    rotate: 80,
+//     color1: "#E278FF",
+//     color2: "#BBF3FD",
+//   },
+//   {
+//     size: 17,
+//     position: { top: 265, left: 60 },
+//     rotate: 80,
 
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
-  {
-    size: 8,
-    position: { top: 350, left: 40 },
-    rotate: 100,
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
-  {
-    size: 18,
-    position: { top: 380, left: 20 },
-    rotate: 90,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 8,
+//     position: { top: 350, left: 40 },
+//     rotate: 100,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 18,
+//     position: { top: 380, left: 20 },
+//     rotate: 90,
 
-    color1: "#BBF3FD",
-    color2: "#E278FF",
-  },
-  {
-    size: 12,
-    position: { top: 330, left: 10 },
-    rotate: 30,
+//     color1: "#BBF3FD",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 12,
+//     position: { top: 330, left: 10 },
+//     rotate: 30,
 
-    color1: "#F8B5D3",
-    color2: "#E278FF",
-  },
-  {
-    size: 8,
-    position: { top: 330, left: 65 },
-    rotate: 30,
-    color1: "#E278FF",
-    color2: "#BBF3FD",
-  },
-  {
-    size: 27,
-    position: { top: 365, left: 80 },
-    rotate: -10,
-    color1: "#E278FF",
-    color2: "#BBF3FD",
-  },
-];
+//     color1: "#F8B5D3",
+//     color2: "#E278FF",
+//   },
+//   {
+//     size: 8,
+//     position: { top: 330, left: 65 },
+//     rotate: 30,
+//     color1: "#E278FF",
+//     color2: "#BBF3FD",
+//   },
+//   {
+//     size: 27,
+//     position: { top: 365, left: 80 },
+//     rotate: -10,
+//     color1: "#E278FF",
+//     color2: "#BBF3FD",
+//   },
+// ];
 
 function App() {
   const phoneNumber = "+972505336334";
